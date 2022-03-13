691cb94444d490e2dfa68f2fc4c10b0b49989b5d
Ravi <ravikumr0403@gmail.com>
2022-03-13 18:29:20 +0530

    feat: document title

--- src/pages/ProductListing.js
+++ src/pages/ProductListing.js
@@ -1,6 +1,8 @@
 import { Filters, Products } from "../components";
+import { useDocumentTitle } from "../hooks/useDocumentTitle";
 
 export const ProductListing = () => {
+  useDocumentTitle("Products | Furnishify");
   return (
     <div className="grid-20-80">
       <Filters />
