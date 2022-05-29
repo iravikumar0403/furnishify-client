eb7ccdae00862a247de9c57a0661729e899185ae
Ravi <ravikumr0403@gmail.com>
2022-05-29 10:11:43 +0530

    feat: add to cart btn on product cart

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,14 +1,14 @@
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { useAuth } from "../context/auth-context";
 import { useProducts } from "../context/product-context";
-import { updateWishlist } from "../services";
+import { updateCart, updateWishlist } from "../services";
 import { findItemById } from "../utils";
 
 export const ProductCard = ({ product }) => {
-  const { _id, images, title, seller, price, rating } = product;
+  const { _id, images, title, price, rating } = product;
   const navigate = useNavigate();
   const { pathname } = useLocation();
-  const { wishlist, dispatch } = useProducts();
+  const { cart, wishlist, dispatch } = useProducts();
   const { user } = useAuth();
 
   const addToWishlist = (product) => {
@@ -31,6 +31,18 @@ export const ProductCard = ({ product }) => {
     updateWishlist(wishlist.filter((item) => item._id !== product._id));
   };
 
+  const addToCart = (product) => {
+    if (user) {
+      dispatch({
+        type: "ADD_TO_CART",
+        payload: product,
+      });
+      updateCart([...cart, { ...product, quantity: 1 }]);
+    } else {
+      navigate("/login", { state: { from: pathname }, replace: true });
+    }
+  };
+
   return (
     <div className="card">
       <Link to={`/products/${_id}`}>
@@ -43,8 +55,7 @@ export const ProductCard = ({ product }) => {
         </div>
       </Link>
       <div className="card-body">
-        <h3 className="card-title">{title}</h3>
-        <small>By {seller}</small>
+        <h3 className="card-title m-0">{title}</h3>
       </div>
       <div className="card-footer flex-between">
         <p className="fs-2 m-0">₹ {price}</p>
@@ -68,6 +79,18 @@ export const ProductCard = ({ product }) => {
           <i className="fa-solid fa-star"></i>
         </div>
       </div>
+      {findItemById(_id, cart) ? (
+        <Link className="btn primary mx-2 mb-2 text-center" to="/cart">
+          Go to cart
+        </Link>
+      ) : (
+        <button
+          className="btn primary mx-2 mb-2"
+          onClick={() => addToCart(product)}
+        >
+          Add to cart
+        </button>
+      )}
     </div>
   );
 };
